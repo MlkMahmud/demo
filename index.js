998735c6b75d215f7cb61f4009440ad6b7a0c830
Malik Mahmud <almalikmahmud@gmail.com>
2022-01-15 01:47:53 +0100

    update stuff

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.json({ message: process.env.GIT_TAG });
+  res.json({ message: `Current tag is: ${process.env.GIT_TAG}` });
 }).listen(3000, () => console.log('running on port: 3000'));
